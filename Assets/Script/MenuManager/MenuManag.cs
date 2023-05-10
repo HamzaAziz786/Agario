cd30aa36c1d7032a91d8b78ccb5fb4507debcada
HamzaAziz786 <98694278+HamzaAziz786@users.noreply.github.com>
2023-05-10 04:52:38 -0700

    adsadads

--- Assets/Script/MenuManager/MenuManag.cs
+++ Assets/Script/MenuManager/MenuManag.cs
@@ -4,6 +4,7 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
+#region 1st CharacterSelection
 
 [System.Serializable]
 public struct CharacterMainStruct
@@ -23,16 +24,49 @@ public struct SelectedCharacterDetail
     public List<GameObject> Selectedstars;
     public TMP_Text Selecteddescription;
 }
+
+#endregion
+
+#region SubCharacteDetail
+[System.Serializable]
+public struct SubCharacterSelect
+{
+
+    public string Title;
+    public string Name;
+    public int Stars;
+    public string description;
+}
+[System.Serializable]
+public struct SubCharacterDetail
+{
+    public TMP_Text SelectedCharactername;
+    public TMP_Text SelectedTitle;
+    public TMP_Text SelectedName;
+    public List<GameObject> Selectedstars;
+    public TMP_Text Selecteddescription;
+    public List<TMP_Text> SubCharactersubName;
+    
+} 
+#endregion
 public class MenuManag : MonoBehaviour
 {
-   
-    public static MenuManag instance;
-    public List<CharacterMainStruct> charactermain;
-    public List<SelectedCharacterDetail>selectedChrDetail;
-    
 
-   
-    
+    public static MenuManag instance;
+
+
+    #region CharacterSelection
+    public List<CharacterMainStruct> charactermain;
+    public List<SelectedCharacterDetail> selectedChrDetail;
+    #endregion
+
+    #region SubCharacterSelection
+    public List<SubCharacterSelect> subCharacterSelect;
+    public List<SubCharacterDetail> subCharacterDetail; 
+    #endregion
+
+
+
     private void Awake()
     {
         instance = this;
@@ -52,5 +86,25 @@ public class MenuManag : MonoBehaviour
             selectedChrDetail[0].Selectedstars[i].SetActive(true);
         }
         Debug.Log("Stars" + charactermain[CurrentSelectedCharacter].Stars);
+    }
+
+    public void SubCharacterSelection(int SubCharacterNumber)
+    {
+        subCharacterDetail[0].SelectedCharactername.text = "Character # " + SubCharacterNumber;
+        subCharacterDetail[0].SelectedTitle.text = subCharacterSelect[SubCharacterNumber].Title;
+        subCharacterDetail[0].SelectedName.text = subCharacterSelect[SubCharacterNumber].Name;
+        subCharacterDetail[0].Selecteddescription.text = subCharacterSelect[SubCharacterNumber].description;
+        for (int i = 0; i < subCharacterDetail[0].SubCharactersubName.Count; i++)
+        {
+            subCharacterDetail[0].SubCharactersubName[i].text = "Character # " + SubCharacterNumber;
+        }
+        for (int i = 0; i < 5; i++)
+        {
+            subCharacterDetail[0].Selectedstars[i].SetActive(false);
+        }
+        for (int i = 0; i < subCharacterSelect[SubCharacterNumber].Stars; i++)
+        {
+            subCharacterDetail[0].Selectedstars[i].SetActive(true);
+        }
     }
 }
