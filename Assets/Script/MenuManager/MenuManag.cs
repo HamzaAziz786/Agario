c51726aa7206d528d9118bcca3d03101375befdd
HamzaAziz786 <98694278+HamzaAziz786@users.noreply.github.com>
2023-05-10 00:18:57 -0700

    asdasd

--- Assets/Script/MenuManager/MenuManag.cs
+++ Assets/Script/MenuManager/MenuManag.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -16,18 +17,18 @@ public struct CharacterMainStruct
 [System.Serializable]
 public struct SelectedCharacterDetail
 {
-    public Image CharacterImage;
-    public Text Title;
-    public Text Name;
-    public List<GameObject> stars;
-    public Text description;
+    public Image SelectedCharacterImage;
+    public TMP_Text SelectedTitle;
+    public TMP_Text SelectedName;
+    public List<GameObject> Selectedstars;
+    public TMP_Text Selecteddescription;
 }
 public class MenuManag : MonoBehaviour
 {
    
     public static MenuManag instance;
     public List<CharacterMainStruct> charactermain;
-    public List<SelectedCharacterDetail> selectedChrDetail;
+    public List<SelectedCharacterDetail>selectedChrDetail;
     
 
    
@@ -36,8 +37,20 @@ public class MenuManag : MonoBehaviour
     {
         instance = this;
     }
-    public void SetImageOnClick(int imageNumber)
+    public void SelectCharacter(int CurrentSelectedCharacter)
     {
-        
+        selectedChrDetail[0].SelectedCharacterImage.sprite = charactermain[CurrentSelectedCharacter].CharacterImage;
+        selectedChrDetail[0].SelectedTitle.text = charactermain[CurrentSelectedCharacter].Title;
+        selectedChrDetail[0].SelectedName.text = charactermain[CurrentSelectedCharacter].Name;
+        selectedChrDetail[0].Selecteddescription.text = charactermain[CurrentSelectedCharacter].description;
+        for (int i = 0; i < 5; i++)
+        {
+            selectedChrDetail[0].Selectedstars[i].SetActive(false);
+        }
+        for (int i = 0; i < charactermain[CurrentSelectedCharacter].Stars; i++)
+        {
+            selectedChrDetail[0].Selectedstars[i].SetActive(true);
+        }
+        Debug.Log("Stars" + charactermain[CurrentSelectedCharacter].Stars);
     }
 }
