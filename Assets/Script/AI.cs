648651bae5900c45d871aa4ac184e2ad008aa8c1
HamzaAziz786 <98694278+HamzaAziz786@users.noreply.github.com>
2023-05-20 15:37:40 -0700

    asdasd

--- Assets/Script/AI.cs
+++ Assets/Script/AI.cs
@@ -8,22 +8,21 @@ using UnityEngine.UI;
 public class AI : MonoBehaviour
 {
     public static AI aiInstance;
-    public NavMeshAgent aiController;
+    public Transform aiController;
     public GameObject Player;
     //public Text Scoretext;
     //public Text Masstext;
     public int currrent_enemy_value;
-    public float xRange = 10f;
-    public float yRange = 5f;
-    public float zRange = 10f;
-    public float moveInterval = 2f; // time interval to move to a new random position
+   
     public int mass;
     public int score;
+    public GameObject target;
     Vector3 tmpposition;
+    private float speed = 5f;
     private void Awake()
     {
         aiInstance = this;
-        aiController = GetComponent<NavMeshAgent>();
+        
     }
     private void Start()
     {
@@ -36,59 +35,33 @@ public class AI : MonoBehaviour
 
             throw;
         }
-        
-        //Masstext.text = "Enemy Mass : " +mass.ToString();
-        //Scoretext.text = "Enemy Score : " + score.ToString();
-        tmpposition = this.transform.position;
-        //InvokeRepeating("MoveToRandomPosition", 0f, moveInterval);
+        target = GameObject.FindWithTag("Pickup");
+
+        //tmpposition = this.transform.position;
+
     }
     void Update()
     {
-        aiController.SetDestination(Player.transform.position * Time.deltaTime);
-        //float dist = Vector3.Distance(aiController.transform.position, Player.transform.position);
-        //if ( dist < 90)
-        //{
-        //    aiController.SetDestination(Player.transform.position);
-        //}
-        //else if(dist > 95)
-        //{
-        //    aiController.SetDestination(tmpposition);
-        //}
-        
-    }
-    void MoveToRandomPosition()
-    {
-        // generate new random positions within the range
-        float randomX = Random.Range(this.transform.position.x-xRange, this.transform.position.x + xRange);
-        float randomY = Random.Range(this.transform.position.y - yRange, this.transform.position.y+yRange);
-        float randomZ = Random.Range(this.transform.position.z - zRange, this.transform.position.z+zRange);
+        // aiController.SetDestination(Player.transform.position * Time.deltaTime);
+        target = GameObject.FindWithTag("Pickup");
 
-        // move the object to the new random position
-        transform.position = new Vector3(randomX, randomY, randomZ) ;
+        if (target != null)
+        {
+            // Calculate the direction to the target
+            Vector3 direction = target.transform.position - transform.position;
+            direction.Normalize();
+
+            // Move towards the target
+            transform.Translate(direction * speed * Time.deltaTime);
+        }
     }
+  
     private void OnTriggerEnter(Collider other)
     {
-        //if (other.gameObject.CompareTag("OriginalPlayer"))
-        //{
-        //    PlayerController CollidedObjectPlayerController = other.GetComponent<PlayerController>();
-        //    int userMass = CollidedObjectPlayerController.GetMass();
-        //    if (mass < userMass)
-        //    {
-        //        Destroy(this.gameObject);
-        //    }
-        //    else if (mass > userMass)
-        //    {
-        //        // Destroy(other.gameObject);
-        //        Time.timeScale = 0;
-        //    }
-
-
-
-        //}
-        //else
+       
         if (other.gameObject.CompareTag("Pickup"))
         {
-
+            target = GameObject.FindWithTag("Pickup");
             mass += 1;
             score += 1;
             SpawnController.instance.ScoreText[currrent_enemy_value].text = "Score:"+score.ToString();
@@ -102,8 +75,18 @@ public class AI : MonoBehaviour
                 this.transform.localScale.z + .1f);
             Debug.Log("AI ney kha lea ");
             Destroy(other.gameObject);
-            //Eat();
+            
         }
     }
+    public void FindTarget()
+    {
+        target = GameObject.FindWithTag("Pickup");
 
+        // Check if a game object with the specified tag was found
+        if (target != null)
+        {
+            // Do something with the target game object
+            Debug.Log("Target found: " + target.name);
+        }
+    }
 }
