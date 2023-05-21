bc1b562f043874af6fb41c307d0f28ccd798a8e0
HamzaAziz786 <98694278+HamzaAziz786@users.noreply.github.com>
2023-05-21 03:07:24 -0700

    adasdda

--- Assets/Script/AI.cs
+++ Assets/Script/AI.cs
@@ -4,6 +4,7 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.AI;
 using UnityEngine.UI;
+using static UnityEngine.GraphicsBuffer;
 
 public class AI : MonoBehaviour
 {
@@ -17,21 +18,18 @@ public class AI : MonoBehaviour
     
     
     public string targetTag = "Pickup";
+    public string PlayerTag = "OriginalPlayer";
     private float speed = 5f;
     private GameObject nearestTarget;
 
 
-    #region PlayerDetection
-    public float PlayerFacespeed = 5f;
-    public float detectionRange = 1000;
-    public GameObject targetObject; 
-    #endregion
+   
 
     private void Start()
     {
         try
         {
-            //FindNearestTarget();
+            FindNearestTarget();
             Player = GameObject.FindGameObjectWithTag("OriginalPlayer");
         }
         catch (System.Exception)
@@ -46,25 +44,14 @@ public class AI : MonoBehaviour
     }
     void Update()
     {
-       
-        Vector3 Playerdirection = Player.transform.position - transform.position;
-        float distanceToTarget = Playerdirection.magnitude;
-       
-        if (distanceToTarget <= detectionRange)
+        float PlayerDisctance = Vector3.Distance(transform.position, Player.transform.position);
+        Vector3 PlayerDistance = Player.transform.position - transform.position;
+        PlayerDistance.Normalize();
+        if (PlayerDisctance < 150)
         {
-
-            // Move in the opposite direction
-            if (Player.transform.localScale.x > this.transform.localScale.x)
-            {
-                transform.Translate(-Playerdirection.normalized * speed * Time.deltaTime * 20);
-            }
-            else
-            {
-                transform.Translate(Playerdirection.normalized * speed * Time.deltaTime * 20);
-            }
-
+            transform.Translate(PlayerDistance * speed * Time.deltaTime);
         }
-        else
+        else if(PlayerDisctance >= 150)
         {
             FindNearestTarget();
             if (nearestTarget != null)
@@ -77,6 +64,8 @@ public class AI : MonoBehaviour
                 transform.Translate(direction * speed * Time.deltaTime);
             }
         }
+        
+        
 
     }
     private void FindNearestTarget()
@@ -98,17 +87,7 @@ public class AI : MonoBehaviour
         }
     }
 
-    public void FindPlayerDetection()
-    {
-        Vector3 direction = Player.transform.position - transform.position;
-        float distanceToTarget = direction.magnitude;
-
-        if (distanceToTarget <= detectionRange)
-        {
-            // Move in the opposite direction
-            transform.Translate(-direction.normalized * speed * Time.deltaTime);
-        }
-    }
+   
     private void OnTriggerEnter(Collider other)
     {
        
