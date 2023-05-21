742fcacfdd47eb8fa797633f118f22710fcb246a
HamzaAziz786 <98694278+HamzaAziz786@users.noreply.github.com>
2023-05-21 02:25:17 -0700

    asdasd

--- Assets/Script/AI.cs
+++ Assets/Script/AI.cs
@@ -23,7 +23,7 @@ public class AI : MonoBehaviour
 
     #region PlayerDetection
     public float PlayerFacespeed = 5f;
-    public float detectionRange = 20f;
+    public float detectionRange = 1000;
     public GameObject targetObject; 
     #endregion
 
@@ -31,7 +31,7 @@ public class AI : MonoBehaviour
     {
         try
         {
-            FindNearestTarget();
+            //FindNearestTarget();
             Player = GameObject.FindGameObjectWithTag("OriginalPlayer");
         }
         catch (System.Exception)
@@ -46,12 +46,13 @@ public class AI : MonoBehaviour
     }
     void Update()
     {
-        FindNearestTarget();
+       
         Vector3 Playerdirection = Player.transform.position - transform.position;
         float distanceToTarget = Playerdirection.magnitude;
-
+       
         if (distanceToTarget <= detectionRange)
         {
+
             // Move in the opposite direction
             if (Player.transform.localScale.x > this.transform.localScale.x)
             {
@@ -61,10 +62,11 @@ public class AI : MonoBehaviour
             {
                 transform.Translate(Playerdirection.normalized * speed * Time.deltaTime * 20);
             }
-            
+
         }
         else
         {
+            FindNearestTarget();
             if (nearestTarget != null)
             {
                 // Calculate the direction to the nearest target
